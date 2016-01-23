271671f61b14e4a0ccd99f3b0119bbe501cd43ee
Paul Scott <paul.scotto@gmail.com>
2016-01-23 18:17:33 +0000

    Make mondo pip-installable
    Turns the mondo module into a package for the later-adding of submodules.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+setup(
+    name="mondo",
+    version=VERSION,
+    description="Mondo Banking API Client",
+    author=', '.join((
+        'Tito Miguel Costa',
+        'Simon Vans-Colina <simon@simon.vc>',
+    )),
+    url="https://github.com/simonvc/mondo-python",
+    packages=["mondo"],
+    install_requires=['requests>=2.9'],
+    license="MIT",
+)
